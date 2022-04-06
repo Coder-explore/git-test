dd4dfdb757d95bf5a8be6a307fbb992b7c06e968
ernest <ernestyang168@163.com>
2022-04-06 17:28:45 +0800

    Initial commit

---
+++ src/main/java/GitTest.java
@@ -0,0 +1,10 @@
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("111");
+        System.out.println("222");
+        System.out.println("333");
+        System.out.println("444");
+
+    }
+}
